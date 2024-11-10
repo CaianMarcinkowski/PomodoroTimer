dff5b24ce3b27fb6f20e943923de0389b4c5ca85
Caian Marcinkowski Ferreira <caianferre@gmail.com>
2024-11-10 18:00:38 -0300

    feat: mug styled and stop button add

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -13,12 +13,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -27,10 +30,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.lerp
 import com.example.pomodorotimer.ui.theme.Coffe
 import kotlinx.coroutines.delay
@@ -51,10 +56,11 @@ class MainActivity : ComponentActivity() {
 fun PomodoroTimer() {
     var progress by remember { mutableStateOf(0f) }
     var isTimerRunning by remember { mutableStateOf(false) }
-    var buttonText by remember { mutableStateOf("Iniciar") }
+    var buttonText by remember { mutableStateOf("Start") }
     val totalTimeInSeconds = 2 * 60
     val rounded = 90
     var elapsedTime by remember { mutableStateOf(0f) }
+    val widthFraction = 0.75f
 
     val animatedProgress by animateFloatAsState(
         targetValue = progress,
@@ -71,7 +77,7 @@ fun PomodoroTimer() {
                 progress = elapsedTime / totalTimeInSeconds
             }
             isTimerRunning = false
-            buttonText = "Iniciar"
+            buttonText = "Start"
             progress = 0f
             elapsedTime = 0f
         }
@@ -84,12 +90,24 @@ fun PomodoroTimer() {
     }
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
+        Spacer(modifier = Modifier.weight(0.1f))
 
-        Text(text = formatTime(elapsedTime))
+        Row(
+
+        ){
+            Spacer(modifier = Modifier.weight(0.1f))
+            Text(text = formatTime(elapsedTime))
+            Spacer(modifier = Modifier.weight(0.1f))
+            Text(text = formatTime(elapsedTime))
+            Spacer(modifier = Modifier.weight(0.1f))
+
+        }
 
         Spacer(modifier = Modifier.weight(0.1f))
 
@@ -106,43 +124,104 @@ fun PomodoroTimer() {
         )
         */
 
-        Box(
+        Row (
             modifier = Modifier
                 .fillMaxWidth()
                 .height(500.dp)
-                .clip(RoundedCornerShape(bottomStart = rounded.dp, bottomEnd = rounded.dp))
-                .background(Color.LightGray)
+                .padding(horizontal = 16.dp)
         ) {
             Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.BottomStart)
-                    .fillMaxHeight(animatedProgress)
-                    .background(Coffe)
-                    .clip(RoundedCornerShape(bottomStart = rounded.dp, bottomEnd = rounded.dp))
-            )
+                    .fillMaxWidth(widthFraction)
+                    .height(500.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            bottomStart = rounded.dp,
+                            bottomEnd = rounded.dp,
+                            topStart = 5.dp,
+                            topEnd = 5.dp
+                        )
+                    )
+                    .background(Color.LightGray)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomStart)
+                        .fillMaxHeight(animatedProgress)
+                        .background(Coffe)
+                        .clip(RoundedCornerShape(bottomStart = rounded.dp, bottomEnd = rounded.dp))
+                )
 
-            DrawWaves(
-                progress = animatedProgress,
-                modifier = Modifier.fillMaxSize(),
-                backgroundColor = Coffe,
-                liquidColor = Color.LightGray
-            )
+                DrawWaves(
+                    progress = animatedProgress,
+                    modifier = Modifier.fillMaxSize(),
+                    backgroundColor = Coffe,
+                    liquidColor = Color.LightGray
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .wrapContentWidth(Alignment.Start)
+            ) {
+                Canvas(modifier = Modifier
+                    .fillMaxSize(widthFraction)
+                    .wrapContentWidth(Alignment.Start)
+                ) {
+
+                    val mugWidth = size.width * widthFraction
+                    val mugHeight = size.height
+                    val mugHandleY = mugHeight * 0.2f
+                    val mugHandleX = mugWidth * 0f
+
+                    drawArc(
+                        color = Color.LightGray,
+                        startAngle = -90f,
+                        sweepAngle = 180f,
+                        useCenter = false,
+                        size = Size(450f, 450f),
+                        topLeft = Offset(mugHandleX - 225f, mugHandleY),
+                        style = Stroke(width = 80f)
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.weight(0.1f))
+
+        Row(
+
+        ) {
+            Spacer(modifier = Modifier.weight(0.1f))
+
+            Button(onClick = {
+                if (isTimerRunning) {
+                    isTimerRunning = false
+                    buttonText = "Start"
+                } else {
+                    isTimerRunning = true
+                    buttonText = "Pause"
+                }
+            }) {
+                Text(text = buttonText)
+            }
+            Spacer(modifier = Modifier.weight(0.1f))
+
+            Button(onClick = {
+                isTimerRunning = false
+                buttonText = "Start"
+                progress = 0f
+                elapsedTime = 0f
+            }) {
+                Text(text = "Stop")
+            }
+
+            Spacer(modifier = Modifier.weight(0.1f))
         }
 
         Spacer(modifier = Modifier.weight(0.1f))
 
-        Button(onClick = {
-            if (isTimerRunning) {
-                isTimerRunning = false
-                buttonText = "Iniciar"
-            } else {
-                isTimerRunning = true
-                buttonText = "Pausar"
-            }
-        }) {
-            Text(text = buttonText)
-        }
     }
 }
 
@@ -187,15 +266,13 @@ fun DrawWaves(progress: Float, modifier: Modifier = Modifier, backgroundColor: C
 
             path.moveTo(0f, height)
 
-            val waveColor: Color = lerp(backgroundColor, liquidColor, progress)
+            val waveColor: Color = lerp(backgroundColor, liquidColor, progress.coerceIn(0f, 0.8f))
 
             for (x in 0..width.toInt() step 10) {
                 val waveDirection = sin(2 * PI * (x / width.toFloat()) + waveOffset.value)
 
-
                 val y =
                     (waveYOffset - waveAmplitude * waveDirection ).toFloat()
-
 
                 drawLine(
                     start = Offset(x.toFloat(), waveYOffset),
